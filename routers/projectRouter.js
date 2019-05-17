1d523abc266db3f6541ca3f9471bbca13fc43711
Eli Citron <ebcitron@gmail.com>
2019-05-17 16:47:09 -0500

    Sanity Commit

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -40,37 +40,38 @@ projectRouter.get("/:id", async (req, res) => {
 //Post new Project
 
 projectRouter.post("/", async (req, res) => {
+  console.log("req.body", req.body);
   const { name, description, completed } = req.body;
   try {
     const [id] = await Project.addProject(req.body);
-    const project = await Project.getProject(id);
+    const project = await Project.getProjects(id);
     res.status(200).json({ project });
   } catch (error) {
-    console.log(error);
+    console.log("THE ERROR", error);
     res.status(500).json({ message: "Error adding Project" });
   }
 });
 
 //Put project by ID
 
-projectRouter.put("/:id", async (req, res) => {
-  try {
-    const project = await Project.update(req.params.id, req.body);
-    res.status(200).json({ project });
-  } catch (error) {
-    res.status(500).json({ message: "Error editing Project" });
-  }
-});
+// projectRouter.put("/:id", async (req, res) => {
+//   try {
+//     const project = await Project.update(req.params.id, req.body);
+//     res.status(200).json({ project });
+//   } catch (error) {
+//     res.status(500).json({ message: "Error editing Project" });
+//   }
+// });
 
-//Delete project by ID
+// //Delete project by ID
 
-projectRouter.delete("/:id", async (req, res) => {
-  try {
-    const project = await Project.remove(req.params.id);
-    res.status(200).json({ message: "Project was deleted" });
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting project" });
-  }
-});
+// projectRouter.delete("/:id", async (req, res) => {
+//   try {
+//     const project = await Project.remove(req.params.id);
+//     res.status(200).json({ message: "Project was deleted" });
+//   } catch (error) {
+//     res.status(500).json({ message: "Error deleting project" });
+//   }
+// });
 
 module.exports = projectRouter;
